c7db11753bf5b52ff581eb46a4bdf7dd9482c64e
DLJ7666 <jarguezduran@gmail.com>
2024-05-07 12:24:50 +0200

    creación proyecto YoutubeMiner

---
+++ src/test/java/youtubeminer/VideominerApplicationTests.java
@@ -0,0 +1,13 @@
+package youtubeminer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideominerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
