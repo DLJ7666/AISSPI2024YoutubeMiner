273af7dfef6c505a2b0614adeab386507e5b0b4c
DLJ7666 <jarguezduran@gmail.com>
2024-05-12 19:12:21 +0200

    YoutubeController v1.1.0.alpha

--- src/main/java/youtubeminer/controller/YoutubeController.java
+++ src/main/java/youtubeminer/controller/YoutubeController.java
@@ -2,87 +2,115 @@ package youtubeminer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-import videominer.model.Caption;
-import videominer.model.Channel;
-import videominer.model.Comment;
-import videominer.model.User;
-import videominer.model.Video;
+import org.springframework.web.bind.annotation.*;
+import imports.model.Caption;
+import imports.model.Channel;
+import imports.model.Comment;
+import imports.model.User;
+import imports.model.Video;
+import org.springframework.web.client.HttpClientErrorException;
 import youtubeminer.model.caption.YoutubeCaption;
 import youtubeminer.model.caption.YoutubeCaptionSearch;
-import youtubeminer.model.caption.YoutubeCaptionSnippet;
 import youtubeminer.model.channel.*;
 import youtubeminer.model.comment.*;
 import youtubeminer.model.videoSnippet.*;
-import youtubeminer.service.captionService;
-import youtubeminer.service.channelService;
-import youtubeminer.service.commentService;
-import youtubeminer.service.videoSnippetService;
+import youtubeminer.videoservice.*;
+import youtubeminer.youtubeservice.YoutubeCaptionService;
+import youtubeminer.youtubeservice.YoutubeChannelService;
+import youtubeminer.youtubeservice.YoutubeCommentService;
+import youtubeminer.youtubeservice.YoutubeVideoSnippetService;
 
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/youtubeminer")
 public class YoutubeController {
 
     @Autowired
-    captionService captionService;
+    YoutubeCaptionService captionService;
 
     @Autowired
-    channelService channelService;
+    YoutubeChannelService channelService;
 
     @Autowired
-    commentService commentService;
+    YoutubeCommentService commentService;
 
     @Autowired
-    videoSnippetService videoSnippetService;
+    YoutubeVideoSnippetService videoSnippetService;
+
+    @Autowired
+    CaptionService videoCaptionService;
+
+    @Autowired
+    ChannelService videoChannelService;
+
+    @Autowired
+    CommentService videoCommentService;
+
+    @Autowired
+    UserService videoUserService;
+
+    @Autowired
+    VideoService videoVideoService;
 
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping
-    public void create(String youtubeChannelId) {
+    @PostMapping("/{youtubeChannelId}")
+    public void create(@PathVariable String youtubeChannelId,
+                       @RequestParam(name = "maxVideos", required = false) Integer maxVideos,
+                       @RequestParam(name = "maxComments", required = false) Integer maxComments)
+            throws HttpClientErrorException {
         YoutubeChannel channel = channelService.getYoutubeChannel(youtubeChannelId);
-        Channel canal =new Channel(channel.getSnippet().getTitle(), channel.getSnippet().getDescription(),
-                channel.getSnippet().getPublishedAt());
+        System.out.println(channel);
+        long numMaxVideos = 10L, numMaxComments = 10L;
+        if (maxVideos!=null) {
+            numMaxVideos = maxVideos.longValue();
+        }
+        if (maxComments!=null) {
+            numMaxComments = maxComments.longValue();
+        }
+        Channel canal = videoChannelService.creaCanal(youtubeChannelId, channel.getSnippet().getTitle(),
+                channel.getSnippet().getDescription(), channel.getSnippet().getPublishedAt());
         String uploadsPlayListId = channel.getPlayListIdWrapper().getPlayListsIds().getUploadsPlayListId();
         YoutubeVideoSnippetSearch videoList = videoSnippetService.getYoutubeVideoSnippets(uploadsPlayListId);
         List<YoutubeVideoSnippet> videos = new ArrayList<>(videoList.getItems());
         String videosNextPageToken = videoList.getNextPageToken();
-        while(videosNextPageToken!=null){
+        while(videosNextPageToken!=null && videos.size()<=numMaxVideos) {
             videoList = videoSnippetService.getYoutubeVideoSnippets(uploadsPlayListId,videosNextPageToken);
             videos.addAll(videoList.getItems());
             videosNextPageToken = videoList.getNextPageToken();
         }
-        for(YoutubeVideoSnippet videoSnippet:videos){
-            Video v = new Video();
-            v.setName(videoSnippet.getSnippet().getTitle());
-            v.setDescription(videoSnippet.getSnippet().getDescription());
-            v.setReleaseTime(videoSnippet.getSnippet().getPublishedAt());
+        for(YoutubeVideoSnippet videoSnippet:videos.stream().limit(numMaxVideos).toList()) {
+            Video v = videoVideoService.creaVideo(canal.getId(), videoSnippet.getResourceId().getVideoId(),
+                    videoSnippet.getSnippet().getTitle(), videoSnippet.getSnippet().getDescription(),
+                    videoSnippet.getSnippet().getPublishedAt());
             YoutubeCaptionSearch captionList = captionService.getYoutubeCaptions(
                     videoSnippet.getResourceId().getVideoId());
             videoSnippet.addCaptions(captionList.getItems());
             for(YoutubeCaption caption:videoSnippet.getCaptions()) {
-                Caption subtitulo = new Caption(caption.getSnippet().getName(), caption.getSnippet().getLanguage());
+                Caption subtitulo = videoCaptionService.creaSubtitulo(canal.getId(), v.getId(),caption.getId(),
+                        caption.getSnippet().getName(), caption.getSnippet().getLanguage());
             }
             YoutubeCommentSearch commentList = commentService.getYoutubeComments(
                     videoSnippet.getResourceId().getVideoId());
             List<YoutubeComment> comments = new ArrayList<>(commentList.getItems());
             String commentsNextPageToken = commentList.getNextPageToken();
-            while(commentsNextPageToken!=null) {
+            while(commentsNextPageToken!=null && comments.size()<=numMaxComments) {
                 commentList = commentService.getYoutubeComments(videoSnippet.getResourceId().getVideoId(),
                         commentsNextPageToken);
                 comments.addAll(commentList.getItems());
                 commentsNextPageToken = commentList.getNextPageToken();
             }
-            for(YoutubeComment comment:comments) {
-                User u = new User(comment.getCommentSnippet().getTopLevelComment().getSnippet().getAuthorDisplayName(),
+            for(YoutubeComment comment:comments.stream().limit(numMaxComments).toList()) {
+                User u = videoUserService.creaUsuario(
+                        comment.getCommentSnippet().getTopLevelComment().getSnippet().getAuthorDisplayName(),
                         comment.getCommentSnippet().getTopLevelComment().getSnippet().getAuthorChannelUrl(),
                         comment.getCommentSnippet().getTopLevelComment().getSnippet().getAuthorProfileImageUrl());
-                Comment c = new Comment(comment.getCommentSnippet().getTopLevelComment().getSnippet().getTextOriginal(),
+                Comment c = videoCommentService.creaComentario(canal.getId(), v.getId(),
+                        comment.getCommentSnippet().getTopLevelComment().getId(),
+                        comment.getCommentSnippet().getTopLevelComment().getSnippet().getTextOriginal(),
                         comment.getCommentSnippet().getTopLevelComment().getSnippet().getPublishedAt(), u);
             }
         }
